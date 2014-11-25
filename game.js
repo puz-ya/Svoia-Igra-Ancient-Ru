66253d53d46cccc3e0b7fd4b5c4868589579979f
Puzino Yury <mail>
2014-11-25 19:28:27 +0300

    First git project

---
+++ game.js
@@ -0,0 +1,299 @@
+function q01() {
+
+	//01
+	var str = '<h2>В законе Ярослава Мудрого "за удар рукой, чашею или стаканом полагался штраф 12 гривен, а за удар палицей или жердью - 3 гривны". Следовательно, гораздо неизвинительнее было ударить голою рукою, стаканом или чашей, нежели палицей или самым острым мечом. </h2><h2>Обоснуйте логику законодателя.</h2>';
+
+	var pic = '<img src="./pics/01.gif" />';
+	
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a01').style.color = "blue";
+}
+
+function q02() {
+//02
+	var str = '<h2>Говорят, что Париж - законодатель мод. Однако, когда в 1814 году русские казаки вошли в Париж, они не только не поддались влиянию французской моды, но, наоборот, ввели в моду то, чего раньше французы не носили. </h2><h2>Какую новинку позаимствовали парижские модники у казаков?</h2>';
+	var pic = '<img src="./pics/02.jpg" />';
+	
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a02').style.color = "blue";
+}
+
+function q03() {
+//03
+	var str = '<h2>Сегодня каждый старшеклассник, закончив школу, может продолжить своё образование. Эта возможность связана с событием, которое произошло в 1687 году в Заиконоспасском монастыре в Москве. </h2><h2>Что это было за событие?</h2>';
+	var pic = '<img src="./pics/03.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a03').style.color = "blue";
+}
+
+function q04() {
+//04
+	var str = '<h2>Какой год в истории России был самым коротким и почему?</h2>';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = "";
+	document.getElementById('a04').style.color = "blue";
+
+}
+
+function q05() {
+//05
+	var str = '<h2>"Слово о полку Игореве", величайший памятник древнерусской литературы, и сегодня издаётся многотысячными тиражами. Во многих изданиях воспроизводится древнерусский текст памятника. </h2><h2>Почему в современных изданиях древнерусский текст занимает гораздо больше места, чем в первоначальной рукописи? <br />(Размер шрифта не учитывается).</h2>';
+	var pic = '<img src="./pics/05.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a05').style.color = "blue";
+
+}
+
+function q06() {
+//06
+	var str = '<h2>Какая связь между основателем первого русского университета М.В.Ломоносовым и художественной резьбой по кости?</h2>';
+	var pic = '<img src="./pics/06.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a06').style.color = "blue";
+
+}
+
+function q07() {
+//07
+	var str = '<h2>Кто из учеников Иисуса Христа навсегда обосновался на Заячьем острове?</h2>';
+	var pic = '<img src="./pics/07.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a07').style.color = "blue";
+
+}
+
+function q08() {
+//08
+	var str = '<h2>Правда ли, что Европа родилась в Азии?</h2>';
+	var pic = '<img src="./pics/08.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a08').style.color = "blue";
+
+}
+
+function q09() {
+//09
+	var str = '<h2>Известно, что в странах Азии, например, руками едят плов. </h2><h2>А почему на Руси руками ели рассольник?</h2>';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = "";
+	document.getElementById('a09').style.color = "blue";
+
+}
+
+function q10() {
+//10
+	var str = '<h2>В русских народных сказках часто используется выражение "молочная река, кисельные берега". </h2><h2>Как вы можете объяснить это? Ведь кисель - жидкость, а берега рек в действительности являются твёрдыми и прочными.</h2>';
+	var pic = '<img src="./pics/10.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a10').style.color = "blue";
+
+}
+
+function q11() {
+//11
+	var str = '<h2>В допетровской России орденов не было, но людей награждали. </h2><h2>Не выходя из кабинета, наградите ведущего одной из высшей наград того времени.</h2>';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = "";
+	document.getElementById('a11').style.color = "blue";
+
+}
+
+function q12() {
+//12
+	var str = '<h2>Жителей каких стран в старину на Руси называли немцами?</h2>';
+	var pic = '<img src="./pics/12.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a12').style.color = "blue";
+
+}
+
+function q13() {
+//13
+	var str = '<h2>Летописец, рассказывая о строительстве Успенского собора в московском Кремле, сообщает, что оно велось "въ кружало да въ правило". </h2><h2>Что это означает?</h2>';
+	var pic = '<img src="./pics/13.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a13').style.color = "blue";
+
+}
+
+function q14() {
+//14
+	var str = '<h2>Новорождённых мальчиков принято пеленать, используя голубые ленты, а девочек - розовые. </h2><h2>Откуда пошёл этот обычай?</h2>';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = "";
+	document.getElementById('a14').style.color = "blue";
+
+}
+
+function q15() {
+//15
+	var str = '<h2>Когда князь Святослав пошёл собирать дань со своих земель, он увидел город белокаменный и послал воеводу узнать, будут ли жители платить ему дань. Воевода вернулся и сказал: "Город большой, сытый и богатый. Жители его одеты красиво, обуты в сапоги яловые". Тогда князь сказал: "Нет, не будут они платить дань. Поедем, поищем получше". </h2><h2>Кого поехал искать князь?</h2>';
+	var pic = '<img src="./pics/15.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a15').style.color = "blue";
+
+}
+
+function q16() {
+//16
+	var str = '<h2>Кого в Ростове Великом нарекли именами Лебедь, Голодарь, Козёл, Сысой?</h2>';
+	var pic = '<img src="./pics/16.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a16').style.color = "blue";
+
+}
+
+function q17() {
+//17
+	var str = '<h2>Кто был изображён на русской копейке, введённой в обращение после денежной реформы 1535-1538 годов?</h2>';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = "";
+	document.getElementById('a17').style.color = "blue";
+
+}
+
+function q18() {
+//18
+	var str = '<h2>Русские мастерицы издревна славились своим искусством вышивки. Один из видов - шитьё золотом (золотое шитьё). </h2><h2>Объясните, как вышивали мастерицы узоры золотыми и серебряными нитями, если известно, что эти нити в иголку не вдевались.</h2>';
+	var pic = '<img src="./pics/18.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a18').style.color = "blue";
+
+}
+
+function q19() {
+//19
+	var str = '<h2>Почему главный собор Санкт-Петербурга, основателем которого был Пётр I, носит не его имя, а имя Святого Исаакия Долматского?</h2>';
+	var pic = '<img src="./pics/19.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a19').style.color = "blue";
+
+}
+
+function q20() {
+//20
+	var str = '<h2>Русский царь Алексей Михайлович на книге, посвящённой соколиной охоте, сделал надпись, которая впоследствии стала известной русской пословицей. </h2><h2>Воспроизведите эту надпись.</h2>';
+	var pic = '<img src="./pics/20.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a20').style.color = "blue";
+
+}
+
+function q21() {
+//21
+	var str = '<h2>В прошлые века ордена и награды в России носили девизы. Например: "За доблесть", "За отвагу", "За храбрость" и т.д. Они точно указывали, какие человеческие качества увековечивал данный орден. </h2><h2>Какую заслугу перед Россией прославил Пётр I, учредив орден, девизом которого стали слова "За любовь и Отечество"?</h2>';
+	var pic = '<img src="./pics/21.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a21').style.color = "blue";
+
+}
+
+function q22() {
+//22
+	var str = '<h2>Какая связь между русскими стрельцами и итальянским сольдо?</h2>';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = "";
+	document.getElementById('a22').style.color = "blue";
+
+}
+
+function q23() {
+//23
+	var str = '<h2>"Достопочтимые бояре! Извольте откушать с нашего стола ухи грибной, мясной, куриной!" </h2><h2>Есть ли здесь ошибка или всё сказано верно?</h2>';
+	var pic = '<img src="./pics/23.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a23').style.color = "blue";
+
+}
+
+function q24() {
+//24
+	var str = '<h2>В "Хождении за три моря" Афанасий Никитин пишет: "Написал я грешное своё хождение за три моря: первое море - Дербенское, второе море - Индийское и третье море - Стамбульское". </h2><h2>По каким морям нужно проплыть современному мореплавателю, чтобы повторить маршрут Афанасия Никитина?</h2>';
+	var pic = '<img src="./pics/24.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a24').style.color = "blue";
+
+}
+
+function q25() {
+//25
+	var str = '<h2>Почему некоторые башни московского Кремля имеют в плане круглую форму, а некоторые - четырёхугольную?</h2>';
+	var pic = '<img src="./pics/25.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a25').style.color = "blue";
+
+}
+
+function q26() {
+//26
+	var str = '<h2>Памятник древнерусской письменности "Поучение Владимира Мономаха" начинается такими словами: "...Сидя на санях, помыслил я в душе своей и воздал хвалу Богу, который меня до этих дней, грешного, сохранил. Дети мои или иной кто, слушая эту грамотку, не посмейтесь, но кому из детей моих она будет люба, пусть примет её в сердце своё и не станет лениться, а будет трудиться". </h2><h2>"Сидя на санях..." Почему начинает своё "Поучение" с таких слов? Согласитесь, что сани - это несколько странное и неудобное место для литературной работы.</h2>';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = "";
+	document.getElementById('a26').style.color = "blue";
+
+}
+
+function q27() {
+//27
+	var str = '<h2>В 1812 году Наполеон вторгся в Россию. К нему для переговоров прибыл генерал Балашов. "Каков самый подходящий путь на Москву?" - спросил Наполеон. Балашов ответил: "Есть много дорог на Москву, одна из них ведёт через...". </h2><h2>Какой город назвал Балашов?</h2>';
+	var pic = '<img src="./pics/27.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a27').style.color = "blue";
+
+}
+
+function q28() {
+//28
+	var str = '<h2>Русские и византийские иконы с течением десятилетий тускнели и темнели, поэтому нередко поверх старой потемневшей иконы писалась новая. </h2><h2>В чём причина изменения цвета иконы?</h2>';
+	var pic = '<img src="./pics/28.jpg" />';
+
+	document.getElementById('top-element').innerHTML = str;
+	document.getElementById('bottom-element').innerHTML = pic;
+	document.getElementById('a28').style.color = "blue";
+
+}
